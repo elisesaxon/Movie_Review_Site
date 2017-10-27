aca92359af53ee49aa050ae519ff5ee89c9d1ba9
elisesaxon <elisesaxon@gmail.com>
2017-10-26 18:16:22 -0700

    Add initial files

---
+++ tomayto.php
@@ -0,0 +1,19 @@
+<!--
+  Elise Saxon
+  CS342: Web Scripting
+  Due Nov 7, 2017
+
+  Tomayto/tomahto website: A site formatted based on Homework #3
+  specifications. Practice with PHP concepts.
+  This file reads in data from various movie data files and is
+  styled by tomayto.css.
+-->
+
+<html>
+<head>
+  <title>Tomaytos/Tomahtos</title>
+</head>
+<body>
+
+</body>
+</html>
