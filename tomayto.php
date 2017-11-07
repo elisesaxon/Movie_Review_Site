945b9cdcee4b0c1b6b838cb0d4a4f94586b13b83
elisesaxon <elisesaxon@gmail.com>
2017-11-06 19:56:22 -0800

    Minimize usage of echo

--- tomayto.php
+++ tomayto.php
@@ -18,59 +18,67 @@
 </head>
 <body>
 
-  <?php
-    function printTitle() {
+  <?php function printTitle() {
       $titlefile = fopen($_POST["movie"]."/info.txt", 'r');
       $title = fgets($titlefile);
       $year = trim(fgets($titlefile));
       fclose($titlefile);
       echo "<p id=movie_title> $title ($year) </p>";
-    }
+    } ?>
 
-    function printDropDown() {
-       echo "<form method=post action=tomayto.php>";
-       echo "<select name=movie>";
-       $movies = glob("moviefiles/*", GLOB_BRACE);
-       foreach($movies as $movie) {
-         $info = fopen($movie . "/info.txt", 'r');
-         $title = fgets($info);
-         fclose($info);
-         if(strcmp($title, "The Princess Bride") == 0) {
-           echo "<option value=".$movie." selected='selected'>".$title."</option>";
-         } else {
-           echo "<option value=".$movie.">".$title."</option>";
+
+
+    <?php function printTopBar($avg) { ?>
+      <p id=top_bar>
+      <img src=images/tomato.png alt=tomato />
+      <?php
+        echo "$avg";
+        printDropDown();
+      ?>
+      </p>
+    <?php } ?>
+
+    <?php function printDropDown() { ?>
+       <form method=post action=tomayto.php>
+       <select name=movie>
+       <?php
+         $movies = glob("moviefiles/*", GLOB_BRACE);
+         foreach($movies as $movie) {
+           $info = fopen($movie . "/info.txt", 'r');
+           $title = fgets($info);
+           fclose($info);
+           if(strcmp($movie, "moviefiles/princessbride") == 0) {
+             echo "<option value=$movie selected>$title</option>";
+           } else {
+             echo "<option value=$movie>$title</option>";
+           }
          }
-       }
-       echo "<option name=movie> </option>";
-       echo "</select>";
-       echo "<input type=submit name=submitMovie value=Select />";
-    }
+       ?>
+       </select>
+       <input type=submit value=Select />
+    <?php } ?>
 
-    function printTopBar($avg) {
-      echo"<p id=top_bar>";
-      echo "<img src=images/tomato.png alt=tomato />";
-      echo "$avg";
-      printDropDown();
-      echo "</p>";
-    }
+    <?php function printOverview() { ?>
+      <div id=right_bar>
+      <?php
+        $alt = str_replace("moviefiles/", "", $_POST["movie"]);
+        echo "<p> <img src=".$_POST["movie"]."/overview.png alt=$alt/> </p>";
+      ?>
+      <dl>
+      <?php
+        $overviewfile = fopen($_POST["movie"]."/overview.txt", 'r');
+        while(($line = fgets($overviewfile)) !== false) {
+          $linearray = explode(':', $line);
+          echo "<dt>$linearray[0]:</dt>";
+          echo "<dd>$linearray[1]</dd>";
+        }
+        fclose($overviewfile);
+      ?>
+      </dl>
+      </div>
+    <?php } ?>
 
-    function printOverview() {
-      echo "<div id=right_bar>";
-      $alt = str_replace("moviefiles/", "", $_POST["movie"]);
-      echo "<p> <img src=".$_POST["movie"]."/overview.png alt=$alt/> </p>";
-      echo "<dl>";
-      $overviewfile = fopen($_POST["movie"]."/overview.txt", 'r');
-      while(($line = fgets($overviewfile)) !== false) {
-        $linearray = explode(':', $line);
-        echo "<dt>$linearray[0]:</dt>";
-        echo "<dd>$linearray[1]</dd>";
-      }
-      fclose($overviewfile);
-      echo "</dl>";
-      echo "</div>";
-    }
-
-    function printCol($colnum, $colstart, $colend, $reviews) {
+    <?php function printCol($colnum, $colstart, $colend, $reviews) {
         echo "<div id=reviews_col" . $colnum . ">";
         for($i = $colstart; $i < $colend; $i++) {
           $reviewfile = fopen($reviews[$i], 'r');
@@ -78,50 +86,49 @@
           $tomatos = fgets($reviewfile);
           $reviewer = fgets($reviewfile);
           $reviewerfrom = fgets($reviewfile);
-          fclose($reviewfile);
+          fclose($reviewfile); ?>
 
-          echo "<div class=review>";
-          echo "<p>";
-          for($j = 0; $j < $tomatos; $j++) {
-            echo "<img src=images/tomatosmall.png alt=tomatosmall/>";
-          }
+          <div class=review>
+          <p>
+          <?php for($j = 0; $j < $tomatos; $j++) { ?>
+            <img src=images/tomatosmall.png alt=tomatosmall/>
+          <?php }
           echo "$reviewtext </p>";
           echo "<p>$reviewer</p>";
-          echo "<p>$reviewerfrom</p>";
-          echo "</div>";
-        }
-      echo "</div>";
-    }
+          echo "<p>$reviewerfrom</p>"; ?>
+          </div>
+        <?php } ?>
+      </div>
+    <?php } ?>
 
 
-    echo "<h1>Tomayto/Tomahto</h1>";
+    <h1>Tomayto/Tomahto</h1>
 
-    printTitle();
+    <?php printTitle(); ?>
 
-    echo "<div id=main_block>";
+    <div id=main_block>
 
+    <?php
+      printOverview();
 
-    printOverview();
+      $reviews = glob($_POST["movie"]."/review*.txt", GLOB_BRACE);
 
-    $reviews = glob($_POST["movie"]."/review*.txt", GLOB_BRACE);
+      $avg = 0;
+      foreach($reviews as $review) {
+        $reviewarray = file($review);
+        $avg = $avg + (int)$reviewarray[1];
+      }
+      $avg = $avg / sizeof($reviews);
+      printTopBar(round($avg, 1));
 
-    $avg = 0;
-    foreach($reviews as $review) {
-      $reviewarray = file($review);
-      $avg = $avg + (int)$reviewarray[1];
-    }
-    $avg = $avg / sizeof($reviews);
-    printTopBar(round($avg, 1));
+      $maxcol = ceil(sizeof($reviews)/2);
+      printCol(1, 0, $maxcol, $reviews);
+      printCol(2, $maxcol, sizeof($reviews), $reviews);
+    ?>
 
-    $maxcol = ceil(sizeof($reviews)/2);
-    printCol(1, 0, $maxcol, $reviews);
-    printCol(2, $maxcol, sizeof($reviews), $reviews);
+    </div>
 
+    <h1>Tomayto/Tomahto</h1>
 
-
-    echo "</div>";
-
-    echo "<h1>Tomayto/Tomahto</h1>";
-  ?>
 </body>
 </html>
